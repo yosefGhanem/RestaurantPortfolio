3a79a85c3caba329cc849c8c993ff16c1810d90b
yosefGhanme <yousefghanem.linkedin@gmail.com>
2023-09-20 15:08:22 +0300

    Prepared for search engines + Added components folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import Header from "./components/Header"
+import Nav from "./components/Nav"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+import logo from "./assets/logo.png"
 
 function App() {
   return (
-    <div className="App">
-      <h2>Homepage</h2>
-    </div>
+    <>
+    <Header>
+      <img src = {logo} alt="Little Lemon Logo"/>
+    </Header>
+    <Nav>
+      <ul>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/menu">Menu</a></li>
+        <li><a href="/reservations">Reservations</a></li>
+        <li><a href="/order-online">Order Online</a></li>
+        <li><a href="/login">Login</a></li>
+      </ul>
+    </Nav>
+    <Main></Main>
+    <Footer>
+    <img src = {logo} alt="Little Lemon Logo"/>
+    <ul>
+      <li>Doormat Navigation</li>
+      <li>Contact</li>
+      <li>Social Media Links</li>
+    </ul>
+    </Footer>
+    </>
   );
 }
 
